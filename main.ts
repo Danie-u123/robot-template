594fcc18fc5e0cda3d8b64a16b7d72ae2fc31a75
Danie-u123 <113040612+Danie-u123@users.noreply.github.com>
2022-10-25 10:49:47 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    pins.servoSetPulse(AnalogPin.P8, 1300)
+    control.waitMicros(20000)
+})
+input.onButtonPressed(Button.AB, function () {
+    pins.servoSetPulse(AnalogPin.P8, 0)
+    control.waitMicros(20000)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.servoSetPulse(AnalogPin.P8, 1700)
+    control.waitMicros(20000)
+})
+basic.forever(function () {
+	
+})
